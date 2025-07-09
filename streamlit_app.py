7a7240a03970ef09c6412ee73f91ca2d7baa7305
Dennis Núñez <dennishnf@gmail.com>
2025-07-09 17:36:26 +0000

    actualizando

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,37 @@
 import streamlit as st
+import torch
+from torchvision import transforms, models
+from PIL import Image
 
-st.title("Hola Mundo")
-st.write("¡Bienvenido a tu primera app con Streamlit!")
+# Cargar el modelo
+model = models.resnet18(pretrained=False)
+num_ftrs = model.fc.in_features
+model.fc = torch.nn.Linear(num_ftrs, 2)
+model.load_state_dict(torch.load('cat_dog_classifier.pth'))
+model.eval()
+
+# Definir las transformaciones para las imágenes
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+# Función para hacer predicciones
+def predict(image):
+    image = transform(image).unsqueeze(0)
+    with torch.no_grad():
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
+    return "dog" if predicted.item() == 0 else "cat"
+
+# Interfaz de Streamlit
+st.title('Clasificador de gatos y perros')
+uploaded_file = st.file_uploader("Elija una imagen...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Imagen subida.', use_column_width=True)
+    st.write("")
+    st.write("Clasificando...")
+    label = predict(image)
+    st.write(f"Esta imagen es de un: {label}")
