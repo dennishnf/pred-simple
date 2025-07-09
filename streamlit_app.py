e80b65d5b1ba4ed6c3532c32b5255e447ac879f1
Dennis Núñez <dennishnf@gmail.com>
2025-07-09 18:24:13 +0000

    actualizando

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ uploaded_file = st.file_uploader("Elija una imagen...", type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     #st.image(image, caption='Imagen subida.', use_column_width=True)
-    st.image(image, caption='Imagen subida.', high=300)
+    st.image(image, caption='Imagen subida.', width=400)
     st.write("")
     st.write("Clasificando...")
     label = predict(image)
